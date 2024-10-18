65631576ac147babd45fff7b7c23cecc15c3316a
tienlx97 <tienlx97@gmail.com>
2024-10-18 23:21:58 +0700

    chore: first commit

---
+++ src/app.module.ts
@@ -0,0 +1,76 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { FilesModule } from './files/files.module';
+import { AuthModule } from './auth/auth.module';
+import databaseConfig from './database/config/database.config';
+import authConfig from './auth/config/auth.config';
+import appConfig from './config/app.config';
+import mailConfig from './mail/config/mail.config';
+import fileConfig from './files/config/file.config';
+import googleConfig from './auth-google/config/google.config';
+import path from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthGoogleModule } from './auth-google/auth-google.module';
+import { I18nModule } from 'nestjs-i18n/dist/i18n.module';
+import { HeaderResolver } from 'nestjs-i18n';
+import { MailModule } from './mail/mail.module';
+import { HomeModule } from './home/home.module';
+import { AllConfigType } from './config/config.type';
+import { SessionModule } from './session/session.module';
+import { MailerModule } from './mailer/mailer.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './database/mongoose-config.service';
+
+const infrastructureDatabaseModule = MongooseModule.forRootAsync({
+  useClass: MongooseConfigService,
+});
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        databaseConfig,
+        authConfig,
+        appConfig,
+        mailConfig,
+        fileConfig,
+        googleConfig,
+      ],
+      envFilePath: ['.env'],
+    }),
+    infrastructureDatabaseModule,
+    I18nModule.forRootAsync({
+      useFactory: (configService: ConfigService<AllConfigType>) => ({
+        fallbackLanguage: configService.getOrThrow('app.fallbackLanguage', {
+          infer: true,
+        }),
+        loaderOptions: { path: path.join(__dirname, '/i18n/'), watch: true },
+      }),
+      resolvers: [
+        {
+          use: HeaderResolver,
+          useFactory: (configService: ConfigService<AllConfigType>) => {
+            return [
+              configService.get('app.headerLanguage', {
+                infer: true,
+              }),
+            ];
+          },
+          inject: [ConfigService],
+        },
+      ],
+      imports: [ConfigModule],
+      inject: [ConfigService],
+    }),
+    UsersModule,
+    FilesModule,
+    AuthModule,
+    AuthGoogleModule,
+    SessionModule,
+    MailModule,
+    MailerModule,
+    HomeModule,
+  ],
+})
+export class AppModule {}
